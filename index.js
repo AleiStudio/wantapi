5efd49b6e1c077a26c15371242f0091ac3c1aae5
universopack.com <dksiao352@gmail.com>
2025-04-03 22:46:57 -0600

    test indexjs

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const WantDiversion = require('./src/diversion');
 const WantHentai = require('./src/hentai');
 const WantReal = require('./src/real');
+const WantReddit = require('./src/reddit');
 
-module.exports = { WantDiversion, WantHentai, WantReal };
+module.exports = { WantDiversion, WantHentai, WantReal, WantReddit };
 
 // require('./test/npm-test.js');
